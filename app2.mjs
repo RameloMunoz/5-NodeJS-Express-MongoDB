87256f5763c39d2abef839af19eda4a36740d9c4
Ramelo <RameloMunoz@gmail.com>
2021-05-07 19:21:35 -0700

    rectangle2

--- app2.mjs
+++ app2.mjs
@@ -1,14 +1,14 @@
-const rect = require('./rectangle');
+import  rect  from './rectangle2.mjs';
 
 function solveRect(l, w) {
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
 
-    rect(l, w, (err, rectangle) => {
+    rect(l, w, (err, rect) => {
         if (err) {
             console.log('ERROR:', err.message);
         } else {
-            console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
-            console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
+            console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rect.area()}`);
+            console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rect.perimeter()}`);
         }
     });
     console.log('This statement is logged after the call to rect()');
